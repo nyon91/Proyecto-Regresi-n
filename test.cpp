b75b9d531fcebfc2dd093e35076cf80cc6611477
juan <juan@juan-Inspiron-5437>
2018-08-09 10:53:50 -0600

    Committed with Git-Upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+    char fecha[25];//ctime devuelve 26 caracteres pero tambien se podría usar un puntero de char
+    time_t current_time;
+    current_time=time(NULL);
+    ctime(&current_time);
+    strcpy(fecha, ctime(&current_time));
+    cout << "Ejecutando las pruebas del " << fecha << endl;
+    return 0;
+}
