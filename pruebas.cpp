745cab8176cd2bcb4e9c5335434f4f78d561ee7c
juan <juan@juan-Inspiron-5437>
2018-08-13 13:41:45 -0600

    committing new configuration changes!

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
 int main(){
   int num, semilla;
+  char cadena[128];
+  //Se crea un fichero
+  ofstream fs("file.txt");
 
+  //Se le pide ingresar una semilla al usuario para hacer mas aleatorio el numegro generado
   cout << "Ingrese una semilla para rand()" <<endl; //nueva linea
   cin >> semilla;
   srand(semilla);
 
+  //Se genera un numero aleatorio
   for(int i=0; i<10; i++){
     num=1+rand() % 50;
-    cout << num << endl;
   }
 
   if(num % 2 == 0){
     num=1;
-    cout << "número par = " << num << endl;
   }
   else{
     num=0;
-    cout << "número impar = " << num << endl;
   }
 
+  //Se envia el numero al archivo file.txt
+  fs << num << endl;
+  //Se cierra el fichero
+  fs.close();
+
+  //Se abre un fichero
+   ifstream fe("file.txt");
+
+   // Leeremos el fichero
+   fe.getline(cadena, 128);
+   cout << cadena << endl;
+
+   return 0;
 }
