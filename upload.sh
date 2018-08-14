9f6388cfcb6955a73ca87394ac5efadc489b7e46
juan <juan@juan-Inspiron-5437>
2018-08-14 08:55:28 -0600

    committing new configuration changes!

---
+++ upload.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /home/juan/Documentos/cron
+./pruebas
+
+for line in $(cat file.txt);
+
+do
+
+var=1
+
+if [ $line -eq $var ]; then
+        cd /home/juan/Documentos/cron
+        git add .
+        git commit -m "committing new configuration changes!"
+        git push origin master
+        echo ""
+        echo "git repo updated"
+else
+        echo "Fallo las pruebas"
+fi;
+
+done
