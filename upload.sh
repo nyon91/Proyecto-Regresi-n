d624feb8bd995674930edd19cc7d00df1b575667
juan <juan@juan-Inspiron-5437>
2018-08-20 14:06:05 -0600

    committing new configuration changes!

--- upload.sh
+++ upload.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+clear all
 cd /home/juan/Documentos/cron
+./nombre
+
+while IFS=' ' read -r line || [[ -n "$line" ]]; do printf ""; done < nombre.txt
+
 ./pruebas
 
-for line in $(cat file.txt);
+for linea in $(cat file.txt);
 
 do
 
 var=1
 
-if [ $line -eq $var ]; then
+if [ $linea -eq $var ]; then
         cd /home/juan/Documentos/cron
-        git add .
+	git add "$line"
+#       git add .
         git commit -m "committing new configuration changes!"
         git push origin master
         echo ""
